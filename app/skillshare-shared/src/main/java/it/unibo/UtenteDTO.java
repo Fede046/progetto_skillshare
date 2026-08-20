37a6db91953e18af80af9482e89eeab80037e166
Bufalox <gabrielebufalo44@gmail.com>
2026-08-20 17:07:51 +0200

    Aggiornato UtenteDTO aggiunti i campi bio, foto e tagCompetenza

--- app/skillshare-shared/src/main/java/it/unibo/UtenteDTO.java
+++ app/skillshare-shared/src/main/java/it/unibo/UtenteDTO.java
@@ -1,6 +1,8 @@
 package it.unibo;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UtenteDTO implements Serializable {
 
@@ -8,9 +10,13 @@ public class UtenteDTO implements Serializable {
     private String password;
     private String nome;
     private String cognome;
+    private String bio;
+    private String photoUrl;
+    private List<String> tagCompetenza;
 
     // Costruttore vuoto OBBLIGATORIO per GWT
     public UtenteDTO() {
+        this.tagCompetenza = new ArrayList<>();
     }
 
     public UtenteDTO(String email, String password, String nome, String cognome) {
@@ -18,6 +24,19 @@ public class UtenteDTO implements Serializable {
         this.password = password;
         this.nome = nome;
         this.cognome = cognome;
+        this.bio = "";
+        this.photoUrl = "";
+        this.tagCompetenza = new ArrayList<>();
+    }
+
+    public UtenteDTO(String email, String password, String nome, String cognome, String bio, String photoUrl, List<String> tagCompetenza) {
+        this.email = email;
+        this.password = password;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.bio = bio != null ? bio : "";
+        this.photoUrl = photoUrl != null ? photoUrl : "";
+        this.tagCompetenza = tagCompetenza != null ? tagCompetenza : new ArrayList<>();
     }
 
     //Getters e Setters
@@ -51,5 +70,28 @@ public class UtenteDTO implements Serializable {
 
     public void setCognome(String cognome) {
         this.cognome = cognome;
+    }
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public List<String> getTagCompetenza() {
+        return tagCompetenza;
+    }
+
+    public void setTagCompetenza(List<String> tagCompetenza) {
+        this.tagCompetenza = tagCompetenza != null ? tagCompetenza : new ArrayList<>();
     }
 }
